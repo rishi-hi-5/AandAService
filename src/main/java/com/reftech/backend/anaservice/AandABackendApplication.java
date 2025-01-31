16c5c3f391b21bab848a4cbb06ac158ce2fcda37
Rushikesh Mukund Fanse <rishikeshfanse@gmail.com>
2025-01-31 20:12:02 +0530

    inital service setup

---
+++ src/main/java/com/reftech/backend/anaservice/AandABackendApplication.java
@@ -0,0 +1,11 @@
+package com.reftech.backend.anaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AandABackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AandABackendApplication.class, args);
+    }
+}
